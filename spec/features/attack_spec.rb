21b14042f3b2a4fa153d9790e34a1bde220c6947
Samir Gossain <samir.gossain@gmail.com>
2017-01-19 19:05:30 +0000

    Stage 24;Tests failing

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature "Attacking" do
+
+  scenario "Attacks Player 2 and returns confirmation" do
+    sign_in_and_play
+    click_button 'Attack!'
+    expect(page).to have_content 'Samir attacked Mittens!'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack!'
+    click_link 'OK'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 describe Player do
   subject(:samir) { Player.new('Samir') }
+  subject(:mittens) { Player.new('Mittens') }
 
   describe '#name' do
     it 'returns the name' do
       expect(samir.name).to eq 'Samir'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(samir.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player by 10' do
+      expect(mittens).to receive(:receive_damage)
+      samir.attack(mittens)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit_points by 10' do
+      expect { mittens.receive_damage }.to change { mittens.hit_points }.by(-10)
     end
   end
 end
