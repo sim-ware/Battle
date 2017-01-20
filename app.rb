cc845fdbfe58861d9f0a01fad40abe2d7c731b7c
Samir Gossain <samir.gossain@gmail.com>
2017-01-20 11:00:13 +0000

    Stage 24: Tests Passing

--- app.rb
+++ app.rb
@@ -8,20 +8,25 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1_name = params[:player_1_name]
-    $player_2_name = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1_name
-    @player_2_name = $player_2_name
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    @player_2_hp = $player_2.hit_points
+    @player_1_hp = $player_1.hit_points
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1_name
-    @player_2_name = $player_2_name
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    $player_1.attack($player_2)
+    @player_1_hp = $player_1.hit_points
+    @player_2_hp = $player_2.hit_points
     erb :attack
   end
 
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -11,8 +11,8 @@ feature "Attacking" do
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack!'
-    
+
     expect(page).not_to have_content 'Mittens: 60HP'
-    expect(page).to have_content 'Mittens: 50HP'
+    expect(page).to have_content 'Samir attacked Mittens! Mittens:50HP'
   end
 end
